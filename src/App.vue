e27368e7d5d57f73d60f930f512c9c99f44adf12
Michael Carter <mc0202480@otc.edu>
2020-12-02 17:34:34 -0600

    Updated Display, Input, and App vue to load data properly.

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,18 @@
   <div id="app">
     <Header title="Check The Weather" />
     <Input />
-    <Display />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Input from './components/Input.vue';
-import Display from './components/Display.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     Input,
-    Display,
   },
 }
 </script>
@@ -25,5 +22,6 @@ export default {
   *{
     margin: 0px;
     padding: 0px;
+    background: 'src/assets/Test.jpg'
   }
 </style>
