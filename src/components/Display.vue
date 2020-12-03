84197ce59315706128ef83f348afcb4f25c0c862
Michael Carter <mc0202480@otc.edu>
2020-12-03 15:12:29 -0600

    Edited formatting on Input.vue and Display.vue

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -8,16 +8,22 @@
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
                 <h1 class="display-4">{{currentWeather.name}}</h1>
-                <h2>Current Conditions</h2>
-                <p class="lead"><strong>Temperature</strong></p>
-                <p>Current Temp: {{curTemp}}<span v-if="unit===false">°C</span><span v-else>°F</span></p>
-                <p>{{currentWeather.weather[0].main}}<br/>
-                <img :src="'http://openweathermap.org/img/wn/' + currentWeather.weather[0].icon + '@2x.png'"></p>
-                <h2>Today's Forcast</h2>
-                <p>High Temp: {{highTemp}}<span v-if="unit===false">°C</span><span v-else>°F</span></p>
-                <p>Low Temp: {{lowTemp}}<span v-if="unit===false">°C</span><span v-else>°F</span></p>
-                <p>Sunrise: {{rise}} A.M.</p>
-                <p>Sunset: {{set}} P.M.</p>
+                <div class="row">
+                    <div class="col">
+                        <h2>Current Conditions</h2>
+                        <p class="lead"><strong>Temperature</strong></p>
+                        <p>Current Temp: {{curTemp}}<span v-if="unit===false">°C</span><span v-else>°F</span></p>
+                        <p>{{currentWeather.weather[0].main}}<br/>
+                        <img :src="'http://openweathermap.org/img/wn/' + currentWeather.weather[0].icon + '@2x.png'"></p>
+                    </div>
+                    <div class="col">
+                        <h2>Today's Forcast</h2>
+                        <p>High Temp: {{highTemp}}<span v-if="unit===false">°C</span><span v-else>°F</span></p>
+                        <p>Low Temp: {{lowTemp}}<span v-if="unit===false">°C</span><span v-else>°F</span></p>
+                        <p>Sunrise: {{rise}} A.M.</p>
+                        <p>Sunset: {{set}} P.M.</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -32,7 +38,7 @@ export default {
 </script>
 
 <style scoped>
-.padding{
+.padding {
     padding: 40px;
 }
 img {
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -4,21 +4,20 @@
     Description: Imported Display.vue and Axios. Once mounted, it defaults to find '65721' zip code. Then the functions of Fahrenheit and Celsius change the units of measurements. Zip code cannot receive MORE than 5 numbers, and it will reset if you try to submit less than 5 numbers. Exported variables from Input to Display to provide the appropriate data for the return.
 -->
 <template>
-<div>
-    <div class="searchBar">
-        <form class="form-inline justify-content-center">
-            <input v-model="zip" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" 
-        maxlength = "5" input type="number" class="form-control mb-2 mr-sm-2" id="zip" placeholder="Zip Code">
-            <div class="btn-toolbar" role="toolbar" aria-label="Grouping buttons">
-                <div class="btn-group mr-2" role="group" aria-label="temp">
-                    <button type="button" class="btn btn-dark" @click=fahrenheit()>ºF</button>
-                    <button type="button" class="btn btn-dark" @click=celsius()>ºC</button>
+    <div>
+        <div class="searchBar">
+            <form class="form-inline justify-content-center">
+                <input v-model="zip" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxlength = "5" input type="number" class="form-control mb-2 mr-sm-2" id="zip" placeholder="Zip Code">
+                <div class="btn-toolbar" role="toolbar" aria-label="Grouping buttons">
+                    <div class="btn-group mr-2" role="group" aria-label="temp">
+                        <button type="button" class="btn btn-dark" @click=fahrenheit()>ºF</button>
+                        <button type="button" class="btn btn-dark" @click=celsius()>ºC</button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
+        <Display :currentWeather="weather" :curTemp="temp" :unit="imperial" :highTemp="maxTemp" :lowTemp="minTemp" :rise="sunrise" :set="sunset"/>
     </div>
-    <Display :currentWeather="weather" :curTemp="temp" :unit="imperial" :highTemp="maxTemp" :lowTemp="minTemp" :rise="sunrise" :set="sunset"/>
-</div>
 </template>
 
 
@@ -46,57 +45,55 @@ export default {
         }
     },
     mounted(){
-    axios.get('https://api.openweathermap.org/data/2.5/weather?zip=65721,us&units=imperial&appid=dc86f2dcaa61375d94a561a001694478')
-    .then( (response) => {
-      this.weather = response.data;
-      this.imperial = true;
-      this.temp = response.data.main.temp.toFixed();
-      this.maxTemp = response.data.main.temp_max.toFixed();
-      this.minTemp = response.data.main.temp_min.toFixed();
-      this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-US").slice(0,4);
-      this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-US").slice(0,4);
-    })
-  },
-    methods: {
-    fahrenheit()
-    {
-        if (this.zip.length != 5){
-            alert('Zip code must contain 5 digits');
-            this.zip = '';
-        }
-        else {
-        axios.get('https://api.openweathermap.org/data/2.5/weather?zip=' + this.zip + ',us&units=imperial&appid=dc86f2dcaa61375d94a561a001694478')
+        axios.get('https://api.openweathermap.org/data/2.5/weather?zip=65721,us&units=imperial&appid=dc86f2dcaa61375d94a561a001694478')
         .then( (response) => {
             this.weather = response.data;
-            this.temp = response.data.main.temp.toFixed();
-            this.maxTemp = response.data.main.temp_max.toFixed();
-            this.minTemp = response.data.main.temp_min.toFixed();
-            this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-US").slice(0,4);
-            this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-US").slice(0,4);
-        });
-        this.imperial = true;
-    }
-    },
-    celsius()
-    {
-        if (this.zip.length != 5){
-            alert('Zip code must contain 5 digits');
-            this.zip = '';
-        }
-        else {
-        axios.get('https://api.openweathermap.org/data/2.5/weather?zip=' +this.zip+ ',us&units=metric&appid=dc86f2dcaa61375d94a561a001694478')
-        .then( (response) => {
-            this.weather = response.data;
+            this.imperial = true;
             this.temp = response.data.main.temp.toFixed();
             this.maxTemp = response.data.main.temp_max.toFixed();
             this.minTemp = response.data.main.temp_min.toFixed();
             this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-US").slice(0,4);
             this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-US").slice(0,4);
         })
-        this.imperial = false;
+    },
+    methods: {
+        fahrenheit(){
+            if (this.zip.length != 5){
+                alert('Zip code must contain 5 digits');
+                this.zip = '';
+            }
+        else {
+            axios.get('https://api.openweathermap.org/data/2.5/weather?zip=' + this.zip + ',us&units=imperial&appid=dc86f2dcaa61375d94a561a001694478')
+            .then( (response) => {
+                this.weather = response.data;
+                this.temp = response.data.main.temp.toFixed();
+                this.maxTemp = response.data.main.temp_max.toFixed();
+                this.minTemp = response.data.main.temp_min.toFixed();
+                this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-US").slice(0,4);
+                this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-US").slice(0,4);
+            });
+            this.imperial = true;
         }
     },
-},
+        celsius(){
+            if (this.zip.length != 5){
+                alert('Zip code must contain 5 digits');
+                this.zip = '';
+            }
+            else {
+                axios.get('https://api.openweathermap.org/data/2.5/weather?zip=' +this.zip+ ',us&units=metric&appid=dc86f2dcaa61375d94a561a001694478')
+                .then( (response) => {
+                    this.weather = response.data;
+                    this.temp = response.data.main.temp.toFixed();
+                    this.maxTemp = response.data.main.temp_max.toFixed();
+                    this.minTemp = response.data.main.temp_min.toFixed();
+                    this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-US").slice(0,4);
+                    this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-US").slice(0,4);
+                })
+                this.imperial = false;
+            }
+        },
+    },
 }
 </script>
 
